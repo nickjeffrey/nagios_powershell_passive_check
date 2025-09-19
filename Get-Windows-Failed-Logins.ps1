f418219a367f2d667c8e976c41cf3ade6a78d665
nickjeffrey <nickjeffrey@users.noreply.github.com>
2025-09-18 18:59:15 -0600

    Add NCPA compatibility

--- Get-Windows-Failed-Logins.ps1
+++ Get-Windows-Failed-Logins.ps1
@@ -1,13 +1,15 @@
 # powershell function to perform check on local machine
-# this script is called from the master nagios_passive_check.ps1 script
-# the results of this check are submitted to the nagios server as a passive check via HTTP
+# this script can be called by NCPA or executed as a passive check
+# intent: find failed user logins in the past 60 minutes
 
 # CHANGE LOG
 # ----------
 # 2022-05-25	njeffrey	Script created
+# 2025-09-18	njeffrey	Add NCPA compatibility
 
 function Get-Windows-Failed-Logins {
    #
+   $verbose = "no"             #yes|no flag to increase verbosity for debugging
    if ($verbose -eq "yes") { Write-Host "" ; Write-Host "Running Get-Windows-Failed-Logins function" }
    #
    # declare variables
@@ -44,27 +46,43 @@ function Get-Windows-Failed-Logins {
    $bad_users = $bad_users | Sort-Object | Get-Unique               #sort the array and eliminate duplicates
    if ($failed_login_count -eq 0) {
       $plugin_state = 0 								 #0=ok 1=warn 2=critical 3=unknown
-      $plugin_output = "$service OK - $failed_login_count failed logins in last hour"
+      $common_output_data = "$service OK - $failed_login_count failed logins in last hour"
    }
    if ( ($failed_login_count -gt 0) -and ($failed_login_count -lt $threshold_warn) ) {
       $plugin_state = 0 								 #0=ok 1=warn 2=critical 3=unknown
-      $plugin_output = "$service OK - $failed_login_count failed logins in last hour.  This is more than zero, but low enough to be acceptable.  Usernames:$bad_users"
+      $common_output_data = "$service OK - $failed_login_count failed logins in last hour.  This is more than zero, but low enough to be acceptable.  Usernames:$bad_users"
    }
    if ($failed_login_count -ge $threshold_warn) {
       $plugin_state = 1 								 #0=ok 1=warn 2=critical 3=unknown
-      $plugin_output = "$service WARN - $failed_login_count failed logins in last hour.  Possible brute force attack. Usernames:$bad_users"
+      $common_output_data = "$service WARN - $failed_login_count failed logins in last hour.  Possible brute force attack. Usernames:$bad_users"
    }
    if ($failed_login_count -ge $threshold_crit) {
       $plugin_state = 2 								 #0=ok 1=warn 2=critical 3=unknown
-      $plugin_output = "$service CRITICAL - $failed_login_count failed logins in last hour.  Possible brute force attack. Usernames:$bad_users"
+      $common_output_data = "$service CRITICAL - $failed_login_count failed logins in last hour.  Possible brute force attack. Usernames:$bad_users"
    }
-   if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-   if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-   return                                                            #break out of function
-} 											#end of function
+   #
+   # capture nagios performance data
+   #
+   $perf_data = "0"  #no performance data to report for this check
+   #
+   # combine the common output and the $perf_data
+   $plugin_output = "$common_output_data | $perf_data"
+   #
+   # print output
+   #
+   if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { 
+      $plugin_state = $exit_code    #used by Submit-Nagios-Passive-Check
+      Submit-Nagios-Passive-Check   #call function to send results to nagios
+   } else {
+      Write-Output "$plugin_output"
+      exit $exit_code
+   }
+   return                                               #break out of function
+} 							#end of function
 #
 # call the above function
 #
 Get-Windows-Failed-Logins
+
 
 

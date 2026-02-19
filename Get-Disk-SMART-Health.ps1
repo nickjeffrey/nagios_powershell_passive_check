82384ccc0b8f8c7e73b999ccc01203d2f4b66d98
nickjeffrey <nickjeffrey@users.noreply.github.com>
2026-02-19 14:59:13 -0700

    add NCPA compatibility

--- Get-Disk-SMART-Health.ps1
+++ Get-Disk-SMART-Health.ps1
@@ -5,6 +5,7 @@
 # CHANGE LOG
 # ----------
 # 2022-05-25	njeffrey	Script created
+# 2026-02-19   add NCPA compatibility
 
 function Get-Disk-SMART-Health {
    #
@@ -16,46 +17,62 @@ function Get-Disk-SMART-Health {
    # declare variables
    $service = "Disk SMART status"                 		#name of check defined on nagios server
    $drive_count = 0						#counter variable used to detect disks that have SMART metrics.  Virtual machines will not have physical disk.
+   $predictfail_count = 0			   #counter variable used to track number of disks with predictive failure warnings
+   #
+   # nagios exit codes
+   $OK       = 0                            	
+   $WARN     = 1                          	
+   $CRITICAL = 2                        
+   $UNKNOWN  = 3                         
+   #
    #
    try {
       Get-WmiObject -namespace root\wmi -class MSStorageDriver_FailurePredictStatus -ErrorAction SilentlyContinue | Foreach $_ {
          $drive_count++						#increment counter 
          if ($verbose -eq "yes") { Write-Host "InstanceName:" $_.InstanceName "PredictFailure:" $_.PredictFailure }
          if ($_.PredictFailure -ne $True) {
-            Write-Host "WARNING: disk smart error"
-            $plugin_state = 1 			 		#0=ok 1=warn 2=critical 3=unknown
-            $plugin_output = "$service WARN - predictive drive failure for disk.  Disk failure is imminent.)"
-            if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results" }
-            if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-            return                                                            #break out of function
+            $predictfail_count++             #increment counter
+            $exit_code = $WARN 			 		#0=ok 1=warn 2=critical 3=unknown
+            $plugin_output = "$service WARN - predictive drive failure for disk $_.InstanceName , disk failure is imminent.)"
+            # BUG ALERT: if there are multiple disks with errors, only the last disk will be shown in the output
          }
       } 							#end of foreach loop
-      if ($drive_count -eq 0) {					#no drives with SMART metrics detected.  Probably a virtual machine, or a physical machine using hardware RAID.
-         $plugin_state = 0 			 		#0=ok 1=warn 2=critical 3=unknown
+      #
+      # this section is for zero drives with SMART metrics detected.  
+      # Probably a virtual machine, or a physical machine using hardware RAID.
+      #
+      if ($drive_count -eq 0) {					
+         $exit_code = $OK 			 		      #0=ok 1=warn 2=critical 3=unknown
          $plugin_output = "$service OK - no drives supporting SMART health metrics were found.  This may be a virtual machine, or a physical machine using hardware RAID."
-         if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results" }
-         if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-         return                                                            #break out of function
       }
-      if ($drive_count -gt 0) {					#found at least 1 drive that supports SMART health metrics
-         # if we get this far, none of the disks have SMART predictive errors
-         $plugin_state = 0 			 		#0=ok 1=warn 2=critical 3=unknown
+      #
+      # at least one drive that supports SMART, no predictive failure errors
+      # if we get to this point far, none of the disks have SMART predictive errors
+      #
+      if ($drive_count -gt 0 -And $predictfail_count -eq 0) {
+         $exit_code = $OK 			 		      #0=ok 1=warn 2=critical 3=unknown
          $plugin_output = "$service OK - no SMART errors detected)"
-         if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results" }
-         if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-         return                                                            #break out of function
       }
-   }								#end of try block
+   }								                  #end of try block
    catch {
       Write-Host "Access denied.  Please check your WMI permissions."
-      $plugin_state = 3 			 #0=ok 1=warn 2=critical 3=unknown
+      $exit_code = $UNKNOWN 			        #0=ok 1=warn 2=critical 3=unknown
       $plugin_output = "$service UNKNOWN - Could not determine drive $driveletter space usage.  Please check WMI permissions of user executing this script."
-      if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results" }
-      if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-      return                                                            #break out of function
+   }                                        #end of catch block
+   #
+   # print output
+   #
+   if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { 
+      $plugin_state = $exit_code    #used by Submit-Nagios-Passive-Check
+      Submit-Nagios-Passive-Check   #call function to send results to nagios
+   } else {
+      Write-Output "$plugin_output"
+      exit $exit_code
    }
+   return                                                            #break out of function
 }								#end of function
 #
 # call the above function
 #
 Get-Disk-SMART-Health
+

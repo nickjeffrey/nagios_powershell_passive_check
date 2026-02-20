b1f0d0bf793532ee5a2f624c17ef1f83b04d74de
nickjeffrey <nickjeffrey@users.noreply.github.com>
2026-02-19 21:37:23 -0700

    add NCPA compatibility

--- Get-Uptime.ps1
+++ Get-Uptime.ps1
@@ -1,10 +1,9 @@
 # powershell function to perform check on local machine
-# this script is called from the master nagios_passive_check.ps1 script
-# the results of this check are submitted to the nagios server as a passive check via HTTP
 
 # CHANGE LOG
 # ----------
-# 2022-05-25	njeffrey	Script created
+# 2022-05-25	njeffrey   Script created
+# 2026-02-19   njeffrey   Add NCPA compatibility
 
 function Get-Uptime {
    #
@@ -13,48 +12,57 @@ function Get-Uptime {
    # declare variables
    $service = "uptime" 	  								#name of check defined on nagios server
    #
+   # nagios exit codes
+   $OK       = 0                            	
+   $WARN     = 1                          	
+   $CRITICAL = 2                        
+   $UNKNOWN  = 3        
+   #
+   #
    $uptime = (get-date) - (gcim Win32_OperatingSystem).LastBootUpTime
-   $uptime = $uptime.TotalMinutes
+   $uptime_minutes = $uptime.TotalMinutes
    if ($verbose -eq "yes") { Write-Host "   uptime is $uptime minutes" }
    #
-   if ($uptime -ge 1440 ) {								#system has been up for more than 1 day (1440 minutes)
-      $uptime = $uptime / 1440								#convert uptime to days
-      $uptime = [math]::round($uptime,0) 		  	                        #truncate to 0 decimal places, nearest day is close enough 
-      $plugin_state = 0 								 #0=ok 1=warn 2=critical 3=unknown
-      $plugin_output = "$service OK - System uptime is $uptime days"
-      if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-      if (Submit-Nagios-Passive-Check) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-      return                                                            #break out of function
+   # figure out if the uptime should be reported in minutes/hours/days
+   #
+   if ($uptime_minutes -ge 1440 ) {								#system has been up for more than 1 day (1440 minutes)
+      $uptime_days = $uptime_minutes / 1440					#convert uptime to days
+      $uptime_days = [math]::round($uptime_days,0) 		#truncate to 0 decimal places, nearest day is close enough 
+      $exit_code = $OK 								 #0=ok 1=warn 2=critical 3=unknown
+      $plugin_output = "$service OK - System uptime is $uptime_days days"
    }
-   if ( ($uptime -gt 60) -and ($uptime -lt 1440) ) {					#system has been up for more than 1 hour, but less than 1 day, so report in hours
-      $uptime = $uptime / 60
-      $uptime = [math]::round($uptime,0) 		  	                        #truncate to 0 decimal places, nearest hour is close enough 
-      $plugin_state = 0 								 #0=ok 1=warn 2=critical 3=unknown
-      $plugin_output = "$service OK - System uptime is $uptime hours"
-      if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-      if (Submit-Nagios-Passive-Check) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-      return                                                            #break out of function
+   if ( ($uptime_minutes -ge 60) -and ($uptime_minutes -lt 1440) ) {		#system has been up for more than 1 hour, but less than 1 day, so report in hours
+      $uptime_hours = $uptime_minutes / 60
+      $uptime_hours = [math]::round($uptime_hours,0) 		  	            #truncate to 0 decimal places, nearest hour is close enough 
+      $exit_code = $OK 								 #0=ok 1=warn 2=critical 3=unknown
+      $plugin_output = "$service OK - System uptime is $uptime_hours hours"
    }
-   if ($uptime -ge 30 ) { 								#system has been up for more than 30 minutes but less than 60 minutes, so report in minutes
-      $uptime = $uptime 
-      $uptime = [math]::round($uptime,0) 		  	                        #truncate to 0 decimal places, nearest minute is close enough 
-      if ($verbose -eq "yes") {Write-Host "OK - System uptime is $uptime minutes" }
-      $plugin_state = 1 								 #0=ok 1=warn 2=critical 3=unknown
-      $plugin_output = "$service OK - System uptime is $uptime minutes"
-      if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-      if (Submit-Nagios-Passive-Check) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-      return                                                            #break out of function
+   if ( ($uptime_minutes -ge 30) -and ($uptime_minutes -lt 60) ) {		#system has been up for more than 30 minutes but less than 60 minutes, so report in minutes
+      $uptime_minutes = $uptime_minutes 
+      $uptime_minutes = [math]::round($uptime_minutes,0) 		  	      #truncate to 0 decimal places, nearest minute is close enough 
+      if ($verbose -eq "yes") {Write-Host "OK - System uptime is $uptime_minutes minutes" }
+      $exit_code = $OK 								 #0=ok 1=warn 2=critical 3=unknown
+      $plugin_output = "$service OK - System uptime is $uptime_minutes minutes"
    }
-   if ($uptime -lt 30 ) {
-      $uptime = $uptime 
-      $uptime = [math]::round($uptime,0) 		  	                        #truncate to 0 decimal places, nearest minute is close enough 
-      if ($verbose -eq "yes") {Write-Host "WARN - recent reboot detected.  System uptime is $uptime minutes" }
-      $plugin_state = 1 								 #0=ok 1=warn 2=critical 3=unknown
-      $plugin_output = "$service WARN - recent reboot detected.  System uptime is $uptime minutes"
-      if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-      if (Submit-Nagios-Passive-Check) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-      return                                                            #break out of function
+   if ($uptime_minutes -lt 30 ) {
+      $uptime_minutes = $uptime_minutes 
+      $uptime_minutes = [math]::round($uptime_minutes,0) 		  	                        #truncate to 0 decimal places, nearest minute is close enough 
+      if ($verbose -eq "yes") {Write-Host "WARN - recent reboot detected.  System uptime is $uptime_minutes minutes" }
+      $exit_code = $WARN 								 #0=ok 1=warn 2=critical 3=unknown
+      $plugin_output = "$service WARN - recent reboot detected.  System uptime is $uptime_minutes minutes"
    }
+   #
+   # print output
+   #
+   if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
+   if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { 
+      $plugin_state = $exit_code    #used by Submit-Nagios-Passive-Check
+      Submit-Nagios-Passive-Check   #call function to send results to nagios
+   } else {
+      Write-Output "$plugin_output"
+      exit $exit_code
+   }
+   return     #break out of function
 } 											#end of function
 #
 # call the above function

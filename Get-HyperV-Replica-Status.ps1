07a0c7ec9cd2ef75ea18b8f2f50d2b023daad89a
nickjeffrey <nickjeffrey@users.noreply.github.com>
2026-02-19 17:10:25 -0700

    add NCPA compatibility

--- Get-HyperV-Replica-Status.ps1
+++ Get-HyperV-Replica-Status.ps1
@@ -5,6 +5,7 @@
 # CHANGE LOG
 # ----------
 # 2022-05-25	njeffrey	Script created
+# 2026-02-29   add NCPA compatibility
 
 function Get-HyperV-Replica-Status {
    #
@@ -12,6 +13,12 @@ function Get-HyperV-Replica-Status {
    #
    # declare variables
    $service       = "Hyper-V Replica"
+   #
+   # nagios exit codes
+   $OK       = 0                            	
+   $WARN     = 1                          	
+   $CRITICAL = 2                        
+   $UNKNOWN  = 3        
    #
    # Confirm the Hyper-V windows feature is installed
    #
@@ -22,21 +29,35 @@ function Get-HyperV-Replica-Status {
       } 
       else {
          if ($verbose -eq "yes") { Write-Host "Hyper-V role is not installed on this machine, skipping check" }
-         $plugin_state = 3 			 					#0=ok 1=warn 2=critical 3=unknown
+         $exit_code = $UNKNOWN 			 					#0=ok 1=warn 2=critical 3=unknown
          $plugin_output = "UNKNOWN - Hyper-V role is not installed on this machine"
+         #
          if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-         if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-         return                                                            #break out of function
-      }
-   }
+         if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { 
+            $plugin_state = $exit_code    #used by Submit-Nagios-Passive-Check
+            Submit-Nagios-Passive-Check   #call function to send results to nagios
+         } else {
+            Write-Output "$plugin_output"
+            exit $exit_code
+         }
+         return   #break out of function
+      }    #end of else block
+   }       #end of try block
    catch {
       Write-Host "ERROR: Could not run Get-WindowsFeature Powershell cmdlet.  Please check permissions."
-      $plugin_state = 3 			 					#0=ok 1=warn 2=critical 3=unknown
+      $exit_code = $UNKNOWN 			 					#0=ok 1=warn 2=critical 3=unknown
       $plugin_output = "UNKNOWN - Could not run Get-WindowsFeature Powershell cmdlet.  Please check permissions."
       if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-      if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-      return                                                            #break out of function
-   }
+      if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { 
+         $plugin_state = $exit_code    #used by Submit-Nagios-Passive-Check
+         Submit-Nagios-Passive-Check   #call function to send results to nagios
+      } else {
+         Write-Output "$plugin_output"
+         exit $exit_code
+      }
+      return   #break out of function   
+   }           #end of catch block
+   #
    #
    # If we get this far, the Hyper-V role is installed.
    #
@@ -45,42 +66,44 @@ function Get-HyperV-Replica-Status {
       $UnhealthyVMs = Measure-VMReplication -ErrorAction Stop | Where-Object {$_.ReplicationMode -eq "Primary" -and $_.ReplicationHealth -ne "Normal"} 
    } 
    catch { 
-      Write-Host -NoNewline "Hyper-V Replica Status is Unknown.|" ; Write-Host "" ; exit $returnStateUnknown 
+      Write-Host -NoNewline "Hyper-V Replica Status is Unknown.|" 
+      Write-Host "" 
+      $exit_code = $UNKNOWN
    } 
    if ($UnhealthyVMs) { 
       # If we have VMs then we need to determine if we need to return critical or warning. 
       $CriticalVMs = $UnhealthyVMs | Where-Object -Property ReplicationHealth -eq "Critical" 
       $WarningVMs  = $UnhealthyVMs | Where-Object -Property ReplicationHealth -eq "Warning" 
       if ($CriticalVMs) { 
-         $plugin_state = 2 			 					#0=ok 1=warn 2=critical 3=unknown
+         $exit_code = $CRITICAL 			 					#0=ok 1=warn 2=critical 3=unknown
          $plugin_output = "$service CRITICAL - Hyper-V Replica Health is critical for $($CriticalVMs.Name)."
-         if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-         if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-         return                                                            #break out of function
       } 
       elseif ($WarningVMs) { 
-         $plugin_state = 1			 					#0=ok 1=warn 2=critical 3=unknown
+         $exit_code = $WARN			 					#0=ok 1=warn 2=critical 3=unknown
          $plugin_output = "$service WARN - Hyper-V Replica Health is WARN for $($WarningVMs.Name)."
-         if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-         if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-         return                                                            #break out of function
       } 
       else { 
-         $plugin_state = 3			 					#0=ok 1=warn 2=critical 3=unknown
+         $exit_code = $UNKNOWN			 					#0=ok 1=warn 2=critical 3=unknown
          $plugin_output = "$service UNKNOWN - Hyper-V Replica Health is UNKNOWN"
-         if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-         if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-         return                                                            #break out of function
       } 
    } 
    else { 
       # No Replication Problems Found 
-      $plugin_state = 3			 						#0=ok 1=warn 2=critical 3=unknown
+      $exit_code = $OK			 						#0=ok 1=warn 2=critical 3=unknown
       $plugin_output = "$service OK - Hyper-V Replica Health is normal"
-      if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-      if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-      return                                                            #break out of function
    } 
+   #
+   # print output
+   #
+   if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
+   if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { 
+      $plugin_state = $exit_code    #used by Submit-Nagios-Passive-Check
+      Submit-Nagios-Passive-Check   #call function to send results to nagios
+   } else {
+      Write-Output "$plugin_output"
+      exit $exit_code
+   }
+   return   
 }												#end of function
 #
 # call the above function

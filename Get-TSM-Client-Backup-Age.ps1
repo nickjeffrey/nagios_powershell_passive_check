d3074685913b6eb3c6343a8ebac6bc8059c4f6cd
nickjeffrey <nickjeffrey@users.noreply.github.com>
2026-02-19 21:25:55 -0700

    Update Get-TSM-Client-Backup-Age.ps1

--- Get-TSM-Client-Backup-Age.ps1
+++ Get-TSM-Client-Backup-Age.ps1
@@ -1,10 +1,9 @@
 # powershell function to perform check on local machine
-# this script is called from the master nagios_passive_check.ps1 script
-# the results of this check are submitted to the nagios server as a passive check via HTTP
 
 # CHANGE LOG
 # ----------
-# 2022-05-25	njeffrey	Script created
+# 2022-05-25	njeffrey   Script created
+# 2026-02-19   njeffrey   Add NCPA compatibility
 
 function Get-TSM-Client-Backup-Age {
    #
@@ -13,17 +12,23 @@ function Get-TSM-Client-Backup-Age {
    # declare variables
    $service = "$processToCheck"   						#name of check defined on nagios server
    #
+   # nagios exit codes
+   $OK       = 0                            	
+   $WARN     = 1                          	
+   $CRITICAL = 2                        
+   $UNKNOWN  = 3        
+   #
    $fileToCheck = "C:\Program Files\Tivoli\TSM\baclient\dsmcad.exe"
    if (-Not(Test-Path $fileToCheck -PathType leaf)) { return }				#break out of function if file does not exist
    if (Test-Path $fileToCheck -PathType leaf) {						#check to see if the file exists
       $processToCheck = "dsmcad.exe"							#
       if (Get-Process $processToCheck) {						#if the file exists, confirm process is running
          if ($verbose -eq "yes") {Write-Host "$processToCheck is running" }
-         $plugin_state = 0 			 					#0=ok 1=warn 2=critical 3=unknown
+         $exit_code = $OK 			 					#0=ok 1=warn 2=critical 3=unknown
          $plugin_output = "$service OK - process $processToCheck is running"
       } else {
          if ($verbose -eq "yes") {Write-Host "WARN: $processToCheck is NOT running" }
-         $plugin_state = 1 								 #0=ok 1=warn 2=critical 3=unknown
+         $exit_code = $WARN 								 #0=ok 1=warn 2=critical 3=unknown
          $plugin_output = "$service WARN - process $processtoCheck is NOT running.  IBM TSM / Spectrum Protect client is installed but not running."
          if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
          if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
@@ -35,26 +40,35 @@ function Get-TSM-Client-Backup-Age {
    # Now let's confirm the backup is less than 24 hours old.
    #
    $fileToCheck = "C:\Program Files\Tivoli\TSM\log\dsmsched.log" 			#this is the default location of the backup logfile
-   if (-Not(Test-Path $fileToCheck -PathType leaf)) { 
-      $plugin_state = 1 								 #0=ok 1=warn 2=critical 3=unknown
+   if (-Not(Test-Path $fileToCheck -PathType leaf)) {                #run this section if $fileToCheck does NOT exist
+      $exit_code = $WARN 								 #0=ok 1=warn 2=critical 3=unknown
       $plugin_output = "$service WARN - process $processtoCheck is running, but the $fileToCheck backup logfile cannot be found."
-      if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-      if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-      return                                                            #break out of function
    }
-   $x = Get-ChildItem $fileToCheck
-   $age_in_hours = (New-TimeSpan -Start (Get-Date $x.LastWriteTime) -End (Get-Date)).TotalHours  #do some math to figure out number of hours between now and license expiration date
-   $age_in_hours = [math]::round($age_in_hours,0)   	                        #truncate to 0 decimal places, nearest hour is close enough
-   if ( $age_in_hours > 28 ) { 								#last backup time was more than 28 hours ago
-      $plugin_state = 1 								 #0=ok 1=warn 2=critical 3=unknown
-      $plugin_output = "$service WARN - last backup was $age_in_hours hours ago."
-      if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
-      if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { Submit-Nagios-Passive-Check}   #call function to send results to nagios
-      return                                                            #break out of function
-   }	
+   if (Test-Path $fileToCheck -PathType leaf) {       #run this section if $fileToCheck exists
+      $x = Get-ChildItem $fileToCheck
+      $age_in_hours = (New-TimeSpan -Start (Get-Date $x.LastWriteTime) -End (Get-Date)).TotalHours  #do some math to figure out number of hours between now and license expiration date
+      $age_in_hours = [math]::round($age_in_hours,0)   	                        #truncate to 0 decimal places, nearest hour is close enough
+      if ( $age_in_hours > 28 ) { 								#last backup time was more than 28 hours ago
+         $exit_code = $WARN 								 #0=ok 1=warn 2=critical 3=unknown
+         $plugin_output = "$service WARN - last backup was $age_in_hours hours ago."
+      }	
+   }
+   #
+   # print output
+   #
+   if ($verbose -eq "yes") { Write-Host "   Submitting nagios passive check results: $plugin_output" }
+   if (Get-Command Submit-Nagios-Passive-Check -errorAction SilentlyContinue) { 
+      $plugin_state = $exit_code    #used by Submit-Nagios-Passive-Check
+      Submit-Nagios-Passive-Check   #call function to send results to nagios
+   } else {
+      Write-Output "$plugin_output"
+      exit $exit_code
+   }
+   return                                                            #break out of function
 } 											#end of function
 #
 # call the above function
 #
 Get-TSM-Client-Backup-Age
+
 
